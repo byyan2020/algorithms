72d36c947414ee88f203f1d6d51a223796be9128
byan <yanbiyu2019@outlook.com>
2022-12-27 20:42:27 -0800

    add

---
+++ 239.sliding-window-maximum.py
@@ -0,0 +1,79 @@
+#
+# @lc app=leetcode id=239 lang=python3
+#
+# [239] Sliding Window Maximum
+#
+# https://leetcode.com/problems/sliding-window-maximum/description/
+#
+# algorithms
+# Hard (46.56%)
+# Likes:    13143
+# Dislikes: 427
+# Total Accepted:    704K
+# Total Submissions: 1.5M
+# Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+#
+# You are given an array of integers nums, there is a sliding window of size k
+# which is moving from the very left of the array to the very right. You can
+# only see the k numbers in the window. Each time the sliding window moves
+# right by one position.
+# 
+# Return the max sliding window.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+# Output: [3,3,5,5,6,7]
+# Explanation: 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+# ⁠1 [3  -1  -3] 5  3  6  7       3
+# ⁠1  3 [-1  -3  5] 3  6  7       5
+# ⁠1  3  -1 [-3  5  3] 6  7       5
+# ⁠1  3  -1  -3 [5  3  6] 7       6
+# ⁠1  3  -1  -3  5 [3  6  7]      7
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [1], k = 1
+# Output: [1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 10^5
+# -10^4 <= nums[i] <= 10^4
+# 1 <= k <= nums.length
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        queue = collections.deque()
+        left, right = 0, 0
+        while right < len(nums):
+            while queue and nums[queue[-1]] < nums[right]:
+                queue.pop()
+            queue.append(right)
+
+            if left > queue[0]:
+                queue.popleft()
+
+            if right + 1 >= k:
+                output.append(nums[queue[0]])
+                left += 1
+            right += 1
+        return output
+        
+# @lc code=end
+
