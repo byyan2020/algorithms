77949ecdb0f7dba7577c8a0a32104d13378439a4
Biyu Yan <yanbiyu2019@outlook.com>
2023-02-16 12:13:50 -0800

    add new

---
+++ 23.merge-k-sorted-lists.py
@@ -0,0 +1,109 @@
+#
+# @lc app=leetcode id=23 lang=python3
+#
+# [23] Merge k Sorted Lists
+#
+# https://leetcode.com/problems/merge-k-sorted-lists/description/
+#
+# algorithms
+# Hard (48.58%)
+# Likes:    15512
+# Dislikes: 582
+# Total Accepted:    1.5M
+# Total Submissions: 3.1M
+# Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+#
+# You are given an array of k linked-lists lists, each linked-list is sorted in
+# ascending order.
+# 
+# Merge all the linked-lists into one sorted linked-list and return it.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+# ⁠ 1->4->5,
+# ⁠ 1->3->4,
+# ⁠ 2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: lists = []
+# Output: []
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: lists = [[]]
+# Output: []
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# k == lists.length
+# 0 <= k <= 10^4
+# 0 <= lists[i].length <= 500
+# -10^4 <= lists[i][j] <= 10^4
+# lists[i] is sorted in ascending order.
+# The sum of lists[i].length will not exceed 10^4.
+# 
+# 
+#
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        """
+        1. Merge two adjacent lists, half the size of lists
+        2. Continue to merge two lists until there are only one list
+
+        time complexity: O(logk * n)
+        space complexity: O(n)
+        """
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            merged_lists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged_lists.append(self.merge_2_lists(l1 , l2))
+            lists = merged_lists
+
+        return lists[0]
+        
+    def merge_2_lists(self, l1, l2):
+        dummy = tail = ListNode()
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        
+        tail.next = l1 if l1 else l2
+
+        return dummy.next
+        
+# @lc code=end
+
